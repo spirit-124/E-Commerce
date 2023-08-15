43ce6b5c9d6df52a1c0039b02a7c7c8584702d41
spirit-124 <bajrangofficial580@gmail.com>
2023-08-15 22:58:37 +0530

    [Added] Filtering in  ProductList Page

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "../features/product-list/ProductListSlice";
+import productReducer from "../features/product-list/productSlice";
 
 export const store = configureStore({
   reducer: {
